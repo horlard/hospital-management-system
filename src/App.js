5463b98f9db674018331e4c5ccbfcbed08ce99c1
horlard <oladipuposaulih@gmail.com>
2021-09-21 11:59:41 +0100

    update: used implemented useState for the modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React,{createContext,useState} from 'react';
 import Header from './components/Header'
 import Patients from './pages/patientsPage'
 import Doctors from './pages/doctorsPage'
 import Appointments from './pages/appointmentsPage'
 import Home from './pages/homePage'
+import Modal from './components/modal';
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,8 +12,20 @@ import {
 } from "react-router-dom";
 
 function App() {
+  const [showModal,setShowModal] = useState(false)
+  const [modalComponent,setModalComponent] = useState(null)
   return (
     <Router>
+    <appContext.Provider
+      value={{
+          openModal: component=> {
+            setModalComponent(component)
+            setShowModal(true)
+          },
+          closeModal:() => setShowModal(false)
+      }}
+    >
+      {showModal ? <Modal>{modalComponent}</Modal> : <></>}
       <Header/>
       <Switch>
         <Route path='/' exact>
@@ -28,8 +41,9 @@ function App() {
           <Appointments/>
         </Route>
       </Switch>
+    </appContext.Provider>
     </Router>
   );
 }
-
+export const appContext = createContext();
 export default App;
