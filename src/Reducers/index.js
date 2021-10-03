a2752cf3d525997a95c6e5e058d89a0bbaa2b7df
horlard <oladipuposaulih@gmail.com>
2021-10-03 11:02:45 +0100

    feat: wired up patientsReducer and DoctorsReducer to be combined

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import Patients from './patientsReducer';
+import Doctors from './doctorsReducer'
 
 const Reducers = combineReducers({
-    Patients
+    Patients,
+    Doctors
 })
 
 export default Reducers;
